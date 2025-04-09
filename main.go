5547e1baf7f35bd75409de8ecd9ddb448ec9ff84
raindrop day11 <3424332463@qq.com>
2025-04-09 18:31:59 +0800

    集成Gin

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello world!")
+	router := gin.New()
+	router.Use(gin.Logger(), gin.Recovery())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	router.Run(":8000")
 }
